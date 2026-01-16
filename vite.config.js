05df81f6a673700a584f9dc7a34a5f89a629e1a5
JavierMeO <150411748+JavierMeO@users.noreply.github.com>
2026-01-15 19:06:38 -0800

    fix: tailwind build working

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  base: '/'
-}
+export default defineConfig({
+  base: '/', // dominio custom
+})
