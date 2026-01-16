46a4cf5f54e568993e33e38138539c89d85bc792
JavierMeO <150411748+JavierMeO@users.noreply.github.com>
2026-01-15 19:41:40 -0800

    sdasd

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/', // dominio custom
-})
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  base: '/'
+}
